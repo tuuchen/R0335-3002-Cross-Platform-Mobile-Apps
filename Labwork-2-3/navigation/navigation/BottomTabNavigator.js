328c5927b22abd48c247cf2a7aa7c02f6dafb689
tuuchen <tuuchen@outlook.com>
2020-10-03 19:14:01 +0300

    Add drawer items and tab icons

--- Labwork-2-3/navigation/navigation/BottomTabNavigator.js
+++ Labwork-2-3/navigation/navigation/BottomTabNavigator.js
@@ -1,13 +1,43 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
 import { HomeStackNavigator, ContactStackNavigator } from "./StackNavigator";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
+const Icons = (props) => {
+  let icon;
+
+  if (props.name === "Home") {
+    icon = props.focused
+      ? "ios-information-circle"
+      : "ios-information-circle-outline";
+  }
+
+  if (props.name === "Contact") {
+    icon = props.focused ? "ios-list-box" : "ios-list";
+  }
+
+  return <Ionicons name={icon} size={props.size} color={props.color} />;
+};
+
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={(navigation) => ({
+        tabBarIcon: (options) => {
+          const props = { ...navigation.route, ...options };
+          return <Icons {...props} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "#9AC4F8",
+        inactiveTintColor: "gray",
+      }}
+      listeners={{
+        tabPress: console.log("hello"),
+      }}
+    >
       <Tab.Screen name="Home" component={HomeStackNavigator} />
       <Tab.Screen name="Contact" component={ContactStackNavigator} />
     </Tab.Navigator>
--- Labwork-2-3/navigation/navigation/DrawerNavigator.js
+++ Labwork-2-3/navigation/navigation/DrawerNavigator.js
@@ -1,17 +1,42 @@
 import React from "react";
 
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
 
-import { ContactStackNavigator } from "./StackNavigator";
 import BottomTabNavigator from "./BottomTabNavigator";
 
 const Drawer = createDrawerNavigator();
 
+const CustomDrawerContent = (props) => {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Home"
+        onPress={() => props.navigation.navigate("Home")}
+      />
+      <DrawerItem
+        label="Contact"
+        onPress={() => props.navigation.navigate("Contact")}
+      />
+      <DrawerItem
+        label="About"
+        onPress={() => props.navigation.navigate("About")}
+      />
+    </DrawerContentScrollView>
+  );
+};
+
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={BottomTabNavigator} />
-      <Drawer.Screen name="Contact" component={ContactStackNavigator} />
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
+      <Drawer.Screen name="Menu" component={BottomTabNavigator} />
     </Drawer.Navigator>
   );
 };
